fb777c470cb5046d2135d102e248b85d91023c88
TechCamp Pro <117430912+webcamp48@users.noreply.github.com>
2025-02-13 21:37:54 +0500

    Update server.js

--- Back-End/server.js
+++ Back-End/server.js
@@ -28,7 +28,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || https://complete-ecommerce-app-front-end.onrender.com, 
+    origin: process.env.FRONTEND_URL || "https://complete-ecommerce-app-front-end.onrender.com", 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
